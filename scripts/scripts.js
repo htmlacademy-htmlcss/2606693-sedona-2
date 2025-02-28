cd28f3b76758eb8c8258311b795714edfd33e5ed
Oleg <ochlvk@gmail.com>
2025-02-28 18:03:15 +0300

    Добавляет модальное окно, скрипт для его работы
    Добавляет модальное окно, скрипт для его работы, SVG-иконку для поля ввода модальной формы

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
   // Скрытие лейбла внутри инпута при вводе текста
   const subscriptionInput = document.querySelector('#subscription-field')
   const label = document.querySelector('label[for="subscription-field"]')
+
   subscriptionInput.addEventListener('input', ({ target }) => {
     if (target.value.length >= 2) return
     if (target.value.length === 1) {
@@ -9,6 +10,23 @@ document.addEventListener('DOMContentLoaded', function () {
     }
     if (target.value.length === 0) {
       label.classList.remove('visually-hidden')
+    }
+  })
+
+  // Переключение модального окна
+  const modalButton = document.querySelector('.search-button')
+  const modalCloseButton = document.querySelector('.modal-close-button')
+  const modalOverlay = document.querySelector('.modal-container')
+
+  modalButton.addEventListener('click', () => {
+    modalOverlay.classList.remove('modal-container-close')
+  })
+  modalCloseButton.addEventListener('click', () => {
+    modalOverlay.classList.add('modal-container-close')
+  })
+  modalOverlay.addEventListener('click', ({ target }) => {
+    if (!target.closest('.modal')) {
+      target.classList.add('modal-container-close')
     }
   })
 })
