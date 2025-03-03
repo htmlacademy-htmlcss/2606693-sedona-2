e7dfc9da826e76832e46d362136808a0939b9de5
Oleg <ochlvk@gmail.com>
2025-03-03 19:02:42 +0300

    Исправляет замечания
    - убирает лишние неразрывные пробелы
    - добавляет br в подзаголовок
    - добавляет внутренние отступы для секций
    - убирает белый бэкграунд для отдельных секций, дублирующий бэкграунд контейнера
    - добавляет закрытие модального окна по ESC
    - добавляет разметку модального окна внутрь контейнера

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
-  // Скрытие лейбла внутри инпута при вводе текста
   function toggleLabel() {
+    // Скрытие лейбла внутри инпута при вводе текста
     const subscriptionInput = document.querySelector('#subscription-field')
     const label = document.querySelector('label[for="subscription-field"]')
 
@@ -21,17 +21,26 @@ document.addEventListener('DOMContentLoaded', function () {
     const modalCloseButton = document.querySelector('.modal-close-button')
     const modalOverlay = document.querySelector('.modal-container')
 
+    function openModal() {
+      modalOverlay.classList.remove('modal-container-close')
+    }
+
+    function closeModal() {
+      modalOverlay.classList.add('modal-container-close')
+    }
+
     if (!modalButton || !modalCloseButton || !modalOverlay) return
 
-    modalButton.addEventListener('click', () => {
-      modalOverlay.classList.remove('modal-container-close')
-    })
-    modalCloseButton.addEventListener('click', () => {
-      modalOverlay.classList.add('modal-container-close')
-    })
+    modalButton.addEventListener('click', openModal)
+    modalCloseButton.addEventListener('click', closeModal)
     modalOverlay.addEventListener('click', ({ target }) => {
       if (!target.closest('.modal')) {
-        target.classList.add('modal-container-close')
+        closeModal()
+      }
+    })
+    document.addEventListener('keydown', ({ key }) => {
+      if (key === 'Escape') {
+        closeModal()
       }
     })
   }
