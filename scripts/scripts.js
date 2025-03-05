4748c237ce76f6637f54b6b9cbbddd3dc3072d4a
Oleg <ochlvk@gmail.com>
2025-03-05 21:41:33 +0300

    Исправляет замечания
    - задает гриды для стилизации значков в .user-nav-link-fav, для форм (для размещения лейблов внутри инпутов), для списков преимуществ и рекомендаций на главной
    - меняет свойство order флекс-элементов в блоке .advantages-item-text
    - меняет скрипт для .subscription-input, чтобы он отображал плейсхолдер при фокусе
    - убирает z-index для лого сайта

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,17 +1,21 @@
 document.addEventListener('DOMContentLoaded', function () {
   function toggleLabel() {
-    // Скрытие лейбла внутри инпута при вводе текста
+    // Скрытие лейбла внутри инпута при фокусе на поле ввода
     const subscriptionInput = document.querySelector('#subscription-field')
     const label = document.querySelector('label[for="subscription-field"]')
 
-    subscriptionInput.addEventListener('input', ({ target }) => {
-      if (target.value.length >= 2) return
-      if (target.value.length === 1) {
-        label.classList.add('visually-hidden')
-      }
-      if (target.value.length === 0) {
-        label.classList.remove('visually-hidden')
-      }
+    function hideLabel() {
+      label.classList.add('visually-hidden')
+    }
+
+    function showLabel() {
+      label.classList.remove('visually-hidden')
+    }
+
+    subscriptionInput.addEventListener('focusin', hideLabel)
+    subscriptionInput.addEventListener('focusout', () => {
+      if (subscriptionInput.value.length > 0) return
+      showLabel()
     })
   }
 
@@ -21,6 +25,8 @@ document.addEventListener('DOMContentLoaded', function () {
     const modalCloseButton = document.querySelector('.modal-close-button')
     const modalOverlay = document.querySelector('.modal-container')
 
+    if (!modalButton || !modalCloseButton || !modalOverlay) return
+
     function openModal() {
       modalOverlay.classList.remove('modal-container-close')
     }
@@ -28,8 +34,6 @@ document.addEventListener('DOMContentLoaded', function () {
     function closeModal() {
       modalOverlay.classList.add('modal-container-close')
     }
-
-    if (!modalButton || !modalCloseButton || !modalOverlay) return
 
     modalButton.addEventListener('click', openModal)
     modalCloseButton.addEventListener('click', closeModal)
