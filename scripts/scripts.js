0552c7c61f17ac66e7a0790324d18ed2bbb1da57
Oleg <ochlvk@gmail.com>
2025-03-26 11:15:25 +0300

    Добавляет фавиконки, настраивает модальное окно
    Убирает скролл при активном модальном окне, убирает смещение при открытии окна

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -24,15 +24,20 @@ document.addEventListener('DOMContentLoaded', function () {
     const modalButton = document.querySelector('.search-button')
     const modalCloseButton = document.querySelector('.modal-close-button')
     const modalOverlay = document.querySelector('.modal-container')
+    const scrollWidth = calcScrollWidth()
 
     if (!modalButton || !modalCloseButton || !modalOverlay) return
 
     function openModal() {
       modalOverlay.classList.remove('modal-container-close')
+      document.body.style.overflow = 'hidden'
+      document.body.style.marginRight = `${scrollWidth}px`
     }
 
     function closeModal() {
       modalOverlay.classList.add('modal-container-close')
+      document.body.style.overflow = ''
+      document.body.style.marginRight = ''
     }
 
     modalButton.addEventListener('click', openModal)
@@ -47,6 +52,22 @@ document.addEventListener('DOMContentLoaded', function () {
         closeModal()
       }
     })
+
+    function calcScrollWidth() {
+      //Создает невидимый элемент для вычисления ширины скролла в разных браузерах
+      let div = document.createElement('div')
+
+      div.style.width = '50px'
+      div.style.height = '50px'
+      div.style.overflowY = 'scroll'
+      div.style.visibility = 'hidden'
+
+      document.body.appendChild(div)
+      let scrollWidth = div.offsetWidth - div.clientWidth
+      div.remove()
+
+      return scrollWidth
+    }
   }
 
   toggleLabel()
